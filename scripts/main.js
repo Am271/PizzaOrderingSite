15e11b9a306e7f547804b14aa432a1bb480bbbfc
Amitash M S <amitu895@gmail.com>
2021-04-23 17:13:22 +0530

    added css styles to takeaway form

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 var content=document.getElementById('content');
 var menuData= '<table border="border"><tr><th rowspan="2">Pizza name</th><th colspan="3">Price</th></tr><tr><th>Regular</th><th>Medium</th><th>Large</th></tr><tr><td>P1</td><td>Pr1</td><td>Pr2</td><td>Pr3</td></tr><tr><td>P2</td><td>Pr1</td><td>Pr2</td><td>Pr3</td></tr></table>'
 var orderData='<form action=""><label>Name:<input type="text" name="name" ></label> <label>contactNum<input type="text" name="phonenum"></label><label>Address<textarea rows="5" columns="25" name="address"></textarea></label><input type="submit" name="submit"></form>'
-var takeawayData='<form action=""><label><input type="text" name="name">Name</label><label><input type="number" name="number">Phone number</label></form>'
+var takeawayData='<div id="content"><div class="nameholder"><div class="label" id="namelabel">Name</div><div id="input"><input type="text" id="data"></div></div><div class="nameholder"><div class="label" id="contactlabel">Contact</div><div id="input"><input type="text" id="data"></div></div><div class="nameholder"></div></div>'
 function showMenu(){
     content.innerHTML=menuData;
 }
