c70da70e70d73a08b00b7f689546da1f0740ddbc
Amitash M S <amitu895@gmail.com>
2021-04-24 13:21:39 +0530

    centered the delivery, takeaway form and cleaned up some CSS properties

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,10 @@
 var content=document.getElementById('content');
-var orderData='<form action=""><label>Name:<input type="text" name="name" ></label> <label>contactNum<input type="text" name="phonenum"></label><label>Address<textarea rows="5" columns="25" name="address"></textarea></label><input type="submit" name="submit"></form>'
-var takeawayData='<div id="content"><div class="nameholder"><div class="label" id="namelabel">Name</div><div id="input"><input type="text" id="data"></div></div><div class="nameholder"><div class="label" id="contactlabel">Contact</div><div id="input"><input type="text" id="data"></div></div><div class="nameholder"></div></div>'
-content.innerHTML=orderData;
-document.getElementById("delivery").addEventListener("click", showDelivery);
-document.getElementById("takeaway").addEventListener("click", showTakeaway);
+var bgcolor1 = getComputedStyle(document.querySelector('.tab1'))['backgroundColor'];
+var bgcolor2= getComputedStyle(document.querySelector('.tab2'))['backgroundColor'];
+document.getElementById("delivery").addEventListener("click", function() {showTab(bgcolor1, 'flex')});
+document.getElementById("takeaway").addEventListener("click", function() {showTab(bgcolor2, 'none')});
 
-function showDelivery() {
-    var bgcolor = getComputedStyle(document.querySelector('.tab1'))['backgroundColor'];
-    document.getElementsByClassName("content-wrapper")[0].style['backgroundColor'] = bgcolor;
-    content.innerHTML=orderData;
-}
-
-function showTakeaway() {
-    var bgcolor = getComputedStyle(document.querySelector('.tab2'))['backgroundColor'];
-    document.getElementsByClassName("content-wrapper")[0].style['backgroundColor'] = bgcolor;
-    content.innerHTML=takeawayData;
+function showTab(color, dispAttr) {
+    document.getElementById('content').style['backgroundColor'] = color;
+    document.getElementById('addressholder').style['display'] = dispAttr;
 }
